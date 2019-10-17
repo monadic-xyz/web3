f0d9ee710531430b3399212859406b7fc5649358
Alexander Simmerl <a.simmerl@gmail.com>
2019-10-17 12:57:33 +0200

    Deploy

---
+++ docs/_next/static/webpack/static/development/pages/updates.js.76d0c4ba214d67809b9f.hot-update.js
@@ -0,0 +1,51 @@
+webpackHotUpdate("static/development/pages/updates.js",{
+
+/***/ "./pages/updates/1.js":
+/*!****************************!*\
+  !*** ./pages/updates/1.js ***!
+  \****************************/
+/*! exports provided: meta, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "meta", function() { return meta; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var layouts_update__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! layouts/update */ "./layouts/update.js");
+var _jsxFileName = "/home/xla/dev/src/github.com/monadic.xyz/gitcoin-x-radicle/pages/updates/1.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var meta = {
+  releaseDate: new Date(2019, 9, 20),
+  title: 'An example update'
+};
+function Index() {
+  return __jsx(layouts_update__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    meta: meta,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "Some inline content."), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "More inline content."));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=updates.js.76d0c4ba214d67809b9f.hot-update.js.map
---
+++ docs/_next/static/webpack/static/development/pages/updates.js.ff42b39b08611949469a.hot-update.js
@@ -0,0 +1,115 @@
+webpackHotUpdate("static/development/pages/updates.js",{
+
+/***/ "./pages/updates/1.js":
+/*!****************************!*\
+  !*** ./pages/updates/1.js ***!
+  \****************************/
+/*! exports provided: meta, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "meta", function() { return meta; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var layouts_update__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! layouts/update */ "./layouts/update.js");
+var _jsxFileName = "/home/xla/dev/src/github.com/monadic.xyz/gitcoin-x-radicle/pages/updates/1.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var meta = {
+  releaseDate: new Date(2019, 9, 20),
+  title: 'An example update'
+};
+function Index() {
+  return __jsx(layouts_update__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    meta: meta,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "At the end of August 2019, we launched ", __jsx("a", {
+    href: "/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "AccessP2P"), ",  an open educational program to provide the network and knowledge individuals need to transition into the peer-to-peer (P2P) space. Over the course of eight lectures in November, our first cohort will receive an introductory education across a spectrum of P2P theories, concepts, and technologies. As we start to wrap up the application period for our first cohort, we reflected on the founding values of the program and our approach to supporting diversity in the P2P space."), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, "Last year, we recognized a strange juxtaposition: the drive of the peer-to-peer community to subvert the dominant norms of today runs contradictory to the insulated, homogeneous people we see building and creating within this community. We could see that the growth within the P2P space - largely within the blockchain domain - mirrored a similar lack of diversity so often cited in the greater tech sector. How can we truly subvert norms if creators and builders that tend to be \u201Cunderrepresented\u201D continue to be \u201Cunderrepresented\u201D?"), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }, "In its broadest sense, P2P, as the ", __jsx("a", {
+    href: "https://p2pfoundation.net/the-p2p-foundation/about-the-p2p-foundation",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }, "P2P Foundation"), "puts it, is \u201Ca process or dynamic that can be found in many communities and movements self-organising around the co-creation of culture and knowledge.\u201D In the technical sense, P2P computing or networking is \u201Ca distributed application architecture that partitions tasks or workloads between peers\u201D (it\u2019s never felt so appropriate to quote ", __jsx("a", {
+    href: "https://en.wikipedia.org/wiki/Peer-to-peer",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37
+    },
+    __self: this
+  }, "Wikipedia"), "). When we talk about participants in the P2P community, whether from a technical or cultural standpoint, there are common motivations: to challenge centralized web services, subvert business models and governance structures, and increase support for \u201Cthe commons.\u201D And these ambitions require experts and curious individuals who have comprehensive experience in the systems that P2P systems attempt to change. These individuals are intimately aware of the flaws in these systems, understand their community or users\u2019 needs, and can act as an important translator for stakeholders outside P2P. These individuals are essential to the success of P2P projects."), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48
+    },
+    __self: this
+  }, "We wanted to address the lack of diversity within the quickly growing and changing P2P community head on. The normalization and adoption of P2P technologies, afterall, depends on the diversity of those creating them. So, what could we create that could help transition more people - coming from different industries, backgrounds, and perspectives - into the P2P space? How could we answer the questions: what is P2P, what do I need to know, and where can I contribute? AccessP2P was born out of this need for an educational entry point."), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58
+    },
+    __self: this
+  }, "Accessibility is built into AccessP2P\u2019s design: lectures take place in the evening so participants can fulfill full-time jobs; the program meets twice a week to limit impact on obligations outside of work; the cohort is kept small and held in-person to increase comfort among and attention given to participants. Alongside of the cohort, we plan to design an asynchronous infrastructure for communication and support between those involved with AccessP2P. Furthermore, all lecture presentations, additional readings, and videos will be posted online for general public access."), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 68
+    },
+    __self: this
+  }, "In an effort to keep the program accessible, we designed the lectures to require only a limited knowledge of programming by focusing on curating high-level introductions to the projects, technologies, philosophies, and people involved in the P2P space. By giving people a comprehensive introduction to a breadth of information, we believe we can expose people to more topics and current challenges in the space, while giving them the necessary resources for continued self-study and support in areas of interest. The network of lecturers serves the double purpose of mentors for future jobs, technical development, and original projects."), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 79
+    },
+    __self: this
+  }, "At the end of November, we\u2019ll present a new set of contributors to the P2P community. Not only will they have an understanding of where and how they can plug into the P2P community, abut also the tools and resources to deepen their exploration. Importantly, the relationships built within AccessP2P will continue to support cohort participants as they start their own projects, join companies, and recruit others to the P2P space."), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 87
+    },
+    __self: this
+  }, "In Berlin and curious about P2P? We hope you will be part of our effort. Apply for the first AccessP2P cohort via the ", __jsx("a", {
+    href: "http://accessp2p.xyz/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 89
+    },
+    __self: this
+  }, "website"), " by September 22, 2019."));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=updates.js.ff42b39b08611949469a.hot-update.js.map
