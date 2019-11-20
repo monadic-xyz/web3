c0aa29f2350f5b6878e39d9558bd040ad8876cc3
Jez Smith <jezsmith1991@gmail.com>
2019-11-20 19:01:55 +0000

    mobile display changes

--- components/Nav.js
+++ components/Nav.js
@@ -8,7 +8,6 @@ import Link from 'next/link'
                     <Link className="dropdown-item" href="../pages/about"><a ><li>About</li></a></Link>
                     <Link className="dropdown-item" href="https://gitcoin.typeform.com/to/TlO4BH"><a><li>Apply</li></a></Link>
                     <Link className="dropdown-item" href="../pages/schedule"><a><li>Schedule</li></a></Link>
-                    <Link className="dropdown-item" href="#sponsors"><a><li>Sponsors</li></a></Link>
                     <Link className="dropdown-item" href="../pages/contact"><a><li>Contact Us</li></a></Link>
                 </ul>
             </div>
