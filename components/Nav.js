848d584bd0da63b7268cf494e3ba66447a80a783
abbey <abbeytitcomb@gmail.com>
2019-11-20 23:53:19 +0100

    version 2 building off of bounty additions!
    TODO:

--- components/Nav.js
+++ components/Nav.js
@@ -1,18 +1,24 @@
 import Link from 'next/link'
+import {LocationIcon, CalendarIcon} from '../components/Icons'
 
     const Nav = () => (
         <nav>
             <div className="links">
-                <ul className="navList"> 
+                <ul className="navList">
                     <Link className="dropdown-item" href="/#top"><a><li>Home</li></a></Link>
-                    <Link className="dropdown-item" href="../pages/about"><a ><li>About</li></a></Link>
-                    <Link className="dropdown-item" href="https://gitcoin.typeform.com/to/TlO4BH"><a><li>Apply</li></a></Link>
-                    <Link className="dropdown-item" href="../pages/schedule"><a><li>Schedule</li></a></Link>
+                    <Link className="dropdown-item" href=""><a ><li>About</li></a></Link>
+                    <Link className="dropdown-item" href="#speakers"><a><li>Schedule</li></a></Link>
                     <Link className="dropdown-item" href="#sponsors"><a><li>Sponsors</li></a></Link>
-                    <Link className="dropdown-item" href="../pages/contact"><a><li>Contact Us</li></a></Link>
+                    <Link className="dropdown-item" href=""><a><li>Contact Us</li></a></Link>
+                    <Link className="dropdown-item" href="https://gitcoin.typeform.com/to/TlO4BH"><a><li>Apply</li></a></Link>
+                </ul>
+                    <br/>
+                <ul className="navList_sub">
+                     <Link className="dropdown-item" href="../pages/contact"><a><li><LocationIcon/> Boulder, Colorado</li></a></Link>
+                     <Link className="dropdown-item" href="../pages/contact"><a><li><CalendarIcon/> Februrary 13th, 2020</li></a></Link>
                 </ul>
             </div>
         </nav>
     )
-    
+
     export default Nav
