f818a953ac6d272567186383dd25305bc4deb584
abbey <abbeytitcomb@gmail.com>
2019-12-10 17:02:56 +0100

    speaker additions + new agenda

--- components/Nav.js
+++ components/Nav.js
@@ -7,14 +7,14 @@ import {LocationIcon, CalendarIcon} from '../components/Icons'
                 <ul className="navList">
                     <Link className="dropdown-item" href="/#top"><a><li>Home</li></a></Link>
                     <Link className="dropdown-item" href="/schedule"><a><li>Schedule</li></a></Link>
-                    <Link className="dropdown-item" href="#sponsors"><a><li>Sponsors</li></a></Link>
+                    <Link className="dropdown-item" href="/#bottom"><a><li>Sponsors</li></a></Link>
                     <Link className="dropdown-item" href="mailto:abbey@monadic.xyz"><a><li>Contact Us</li></a></Link>
                     <Link className="dropdown-item" href="https://gitcoin.typeform.com/to/TlO4BH"><a><li>Apply</li></a></Link>
                 </ul>
                     <br/>
                 <ul className="navList_sub">
                      <Link className="dropdown-item" href="../pages/contact"><a><li><LocationIcon/> Boulder, Colorado</li></a></Link>
-                     <Link className="dropdown-item" href="../pages/contact"><a><li><CalendarIcon/> Februrary 13th, 2020</li></a></Link>
+                     <Link className="dropdown-item" href="../pages/contact"><a><li><CalendarIcon/> Feburary 13th, 2020</li></a></Link>
                 </ul>
             </div>
         </nav>
