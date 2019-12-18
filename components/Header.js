3280f7fcd75dab05716d3d26dcdb06db5e068746
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-12-18 23:54:49 +0100

    Fix updates

--- components/Header.js
+++ components/Header.js
@@ -11,40 +11,74 @@ export default function Header() {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@SustainOSS" />
         <title>Sustain Web3 : 2/13/20 in Boulder :: Checkout our LineUp</title>
-        <meta name="twitter:title" content="Sustain Web3 : 2/13/20 in Boulder :: Checkout our LineUp" />
-        <meta name="twitter:description" content="we are the architects, builders, designers, researchers building the Open Source web3. we believe that blockchain is a new hope for OSS Sustainability. we are working to build a world" />
+        <meta
+          name="twitter:title"
+          content="Sustain Web3 : 2/13/20 in Boulder :: Checkout our LineUp"
+        />
+        <meta
+          name="twitter:description"
+          content="we are the architects, builders, designers, researchers building the Open Source web3. we believe that blockchain is a new hope for OSS Sustainability. we are working to build a world"
+        />
 
-        <meta name="twitter:image" content="https://res.cloudinary.com/dvargvav9/image/upload/v1575999063/SW3_sngkyk.png" />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/dvargvav9/image/upload/v1575999063/SW3_sngkyk.png"
+        />
 
         <meta property="og:url" content="https://web3.sustainoss.org" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Sustain Web3 : 2/13/20 in Boulder :: Checkout our LineUp" />
-        <meta property="og:description" content="we are the architects, builders, designers, researchers building the Open Source web3. we believe that blockchain is a new hope for OSS Sustainability. we are working to build a world" />
+        <meta
+          property="og:title"
+          content="Sustain Web3 : 2/13/20 in Boulder :: Checkout our LineUp"
+        />
+        <meta
+          property="og:description"
+          content="we are the architects, builders, designers, researchers building the Open Source web3. we believe that blockchain is a new hope for OSS Sustainability. we are working to build a world"
+        />
 
-        <meta property="og:image" content="https://res.cloudinary.com/dvargvav9/image/upload/v1575999063/SW3_sngkyk.png" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dvargvav9/image/upload/v1575999063/SW3_sngkyk.png"
+        />
 
         <link rel="shortcut icon" href="/static/favicon.ico" />
-        <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://bootswatch.com/4/yeti/bootstrap.min.css"
+        />
       </Head>
-      <div style={{ margin: "auto", width: "100%" }} className="row justify-content-center">
+      <div
+        style={{ margin: 'auto', width: '100%' }}
+        className="row justify-content-center"
+      >
         <div className="col-12">
           <Nav />
         </div>
       </div>
-      <div style={{ margin: "auto", width: "65%", position: "relative", right: "15px" }} className="row">
-        <div className="col">
+      <div
+        style={{
+          margin: 'auto',
+          width: '65%',
+          position: 'relative',
+          right: '15px'
+        }}
+        className="row"
+      >
+        <div className="col mt-4">
           <Logo logoUrl="https://res.cloudinary.com/dvargvav9/image/upload/v1575999063/SW3_sngkyk.svg" />
         </div>
       </div>
 
-      <div style={{ margin: "auto", width: "100%" }} className="row justify-content-around justify-content-center">
-        <div className="col-5 offset-1">
-          <h6> 📍 Boulder, Colorado</h6>
+      <div className="container">
+        <div className="row text-center event-info">
+          <div className="col-xs-12 col-sm-12 col-md-6 mt-3">
+            <h6>📍Boulder, Colorado</h6>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 mt-3">
+            <h6>📆Feburary 13th, 2020</h6>
+            <span>(The day before Ethdenver)</span>
+          </div>
         </div>
-        <div className="col-5">
-          <h6> 📆 Feburary 13th, 2020</h6>
-        </div>
-
       </div>
     </header>
   )
